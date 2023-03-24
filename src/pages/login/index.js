9a1dbed6e84e9381e40f81a2570db7f595cce9f4
Ngọc Đức <70329271+ngocducpham@users.noreply.github.com>
2023-03-24 14:28:45 +0700

    init

---
+++ src/pages/login/index.js
@@ -0,0 +1,38 @@
+import PublicLayout from '@/modules/main/PublicLayout';
+import Head from 'next/head';
+import React from 'react';
+import { setCacheAccessToken } from '@/services/userService';
+import { showErrorMessage } from '@/services/notifyService';
+import useFetch from '@/hooks/useFetch';
+import apiConfig from '@/constants/apiConfig';
+import { useRouter } from 'next/router';
+import withAuth from '@/components/withAuth';
+import { accessRouteTypeEnum } from '@/constants';
+import Login from '@/modules/login';
+
+function LoginPage() {
+    const { execute, loading } = useFetch(apiConfig.account.login, {});
+    const router = useRouter();
+
+    const onFinish = (values) => {
+        execute({
+            data: values,
+            onCompleted: (res) => {
+                setCacheAccessToken(res.data?.token);
+                router.replace('/');
+            },
+            onError: ({ message }) => showErrorMessage(message),
+        });
+    };
+
+    return (
+        <PublicLayout>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <Login onFinish={onFinish} loading={loading} />
+        </PublicLayout>
+    );
+}
+
+export default withAuth(LoginPage, accessRouteTypeEnum.NOT_LOGIN);
