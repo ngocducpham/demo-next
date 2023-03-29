e101bc0015b2cafc68aaa6019bf466874fabe2e9
Ngọc Đức <70329271+ngocducpham@users.noreply.github.com>
2023-03-29 15:51:03 +0700

    remove cmt

--- src/components/desktop/landing-page/mainContent/popular/Popular.js
+++ src/components/desktop/landing-page/mainContent/popular/Popular.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './index.module.css';
 
 import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Heading from '@/components/common/heading/Heading';
 import classNames from 'classnames';
 import Image from 'next/image';
